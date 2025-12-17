a36cf9962ddc5f2db4d60954337880a77b3bd4b0
Prexidazie Morara <moraraprexidazie@gmail.com>
2025-12-17 19:56:20 +0300

    completed lab : object and arrey interation

--- index.js
+++ index.js
@@ -114,3 +114,110 @@ function gameObject() {
         },
     };
 }
+
+function numPointsScored(playerName) {
+  const game = gameObject(); //expression
+  const homePlayers = game.home.players; //Object
+  const awayPlayers = game.away.players; //object away
+
+  const homePlayer = homePlayers[playerName]; //playerObjet or undefined
+  const awayPlayer = awayPlayers[playerName];
+
+  if (homePlayer) {
+    return homePlayer.points;
+  }
+  if (awayPlayer) {
+    return awayPlayer.points;
+  }
+}
+
+function shoeSize(playerName) {
+  const game = gameObject(); //expression
+  const homePlayers = game.home.players; //Object
+  const awayPlayers = game.away.players; //object away
+
+  const homePlayer = homePlayers[playerName]; //playerObjet or undefined
+  const awayPlayer = awayPlayers[playerName];
+
+  if (homePlayer) {
+    return homePlayer.shoe;
+  }
+  if (awayPlayer) {
+    return awayPlayer.shoe;
+  }
+}
+
+function teamColors(teamName) {
+  const game = gameObject();
+  const home = game.home;
+
+  const away = game.away;
+
+  if (home.teamName === teamName) {
+    return home.colors;
+  }
+  if (away.teamName === teamName) {
+    return away.colors;
+  }
+}
+
+function teamNames() {
+  const game = gameObject();
+  const home = game.home;
+
+  const away = game.away;
+
+  return [home.teamName, away.teamName];
+}
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+  const home = game.home;
+
+  const away = game.away;
+
+  if (home.teamName === teamName) {
+    const players = home.players; //{}
+    const playerKeys = Object.keys(players); //["Alan Anderson", "Reggie Evens",...]
+    let jerseyNumbers = [];
+    for (let i = 0; i < playerKeys.length; i++) {
+      let playerKey = playerKeys[i]; //Alan Anderson ,Reggie Evens
+      let playerObj = players[playerKey]; //{number:0,shoe:16,...}
+      let jerseyNumber = playerObj.number;
+      jerseyNumbers.push(jerseyNumber);
+    }
+    return jerseyNumbers;
+  }
+  if (away.teamName === teamName) {
+    const players = away.players; //{}
+    const playerKeys = Object.keys(players); //["Alan Anderson", "Reggie Evens",...]
+    let jerseyNumbers = [];
+    for (let i = 0; i < playerKeys.length; i++) {
+      let playerKey = playerKeys[i]; //Alan Anderson ,Reggie Evens
+      let playerObj = players[playerKey]; //{number:0,shoe:16,...}
+      let jerseyNumber = playerObj.number;
+      jerseyNumbers.push(jerseyNumber);
+    }
+    return jerseyNumbers;
+  }
+}
+
+function playerStats(playerName) {
+  const game = gameObject();
+  const home = game.home;
+
+  const away = game.away;
+
+  if (home.players[playerName]) {
+    return home.players[playerName];
+  }
+  if (away.players[playerName]) {
+    return away.players[playerName];
+  }
+}
+
+//hinst get object keys as an array
+//transverse the object using object keys
+function bigShoeRebounds() {
+  return 12;
+}
